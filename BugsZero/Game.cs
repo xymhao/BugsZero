b43a627eb214a1e0b7e5450d7d5085184bd64865
xuyueming <xnysym@outlook.com>
2020-02-02 16:31:59 +0800

    添加项目文件。

---
+++ BugsZero/Game.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugsZero
+{
+    public class Game
+    {
+        List<string> players = new List<string>();
+        int[] places = new int[6];
+        int[] purses = new int[6];
+        bool[] inPenaltyBox = new bool[6];
+
+        Queue popQuestions = new Queue();
+        Queue scienceQuestions = new Queue();
+        Queue sportsQuestions = new Queue();
+        Queue rockQuestions = new Queue();
+
+        int currentPlayer = 0;
+        bool isGettingOutOfPenaltyBox;
+
+        public Game()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                popQuestions.Enqueue("Pop Question " + i);
+                scienceQuestions.Enqueue(("Science Question " + i));
+                sportsQuestions.Enqueue(("Sports Question " + i));
+                rockQuestions.Enqueue(createRockQuestion(i));
+            }
+        }
+
+        public String createRockQuestion(int index)
+        {
+            return "Rock Question " + index;
+        }
+
+        public bool isPlayable()
+        {
+            return (howManyPlayers() >= 2);
+        }
+
+        public bool add(String playerName)
+        {
+            players.Add(playerName);
+            places[howManyPlayers()] = 0;
+            purses[howManyPlayers()] = 0;
+            inPenaltyBox[howManyPlayers()] = false;
+
+            Console.WriteLine(playerName + " was added");
+            Console.WriteLine("They are player number " + players.Count);
+            return true;
+        }
+
+        public int howManyPlayers()
+        {
+            return players.Count;
+        }
+
+        public void roll(int roll)
+        {
+            Console.WriteLine(players[currentPlayer] + " is the current player");
+            Console.WriteLine("They have rolled a " + roll);
+
+            if (inPenaltyBox[currentPlayer])
+            {
+                if (roll % 2 != 0)
+                {
+                    isGettingOutOfPenaltyBox = true;
+
+                    Console.WriteLine(players[currentPlayer] + " is getting out of the penalty box");
+                    movePlayerAndAskQuestion(roll);
+                }
+                else
+                {
+                    Console.WriteLine(players[currentPlayer] + " is not getting out of the penalty box");
+                    isGettingOutOfPenaltyBox = false;
+                }
+
+            }
+            else
+            {
+
+                movePlayerAndAskQuestion(roll);
+            }
+
+        }
+
+        private void movePlayerAndAskQuestion(int roll)
+        {
+            places[currentPlayer] = places[currentPlayer] + roll;
+            if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+
+            Console.WriteLine(players[currentPlayer]
+                    + "'s new location is "
+                    + places[currentPlayer]);
+            Console.WriteLine("The category is " + currentCategory());
+            askQuestion();
+        }
+
+        private void askQuestion()
+        {
+            if (currentCategory() == "Pop")
+                Console.WriteLine(popQuestions.Dequeue());
+            if (currentCategory() == "Science")
+                Console.WriteLine(scienceQuestions.Dequeue());
+            if (currentCategory() == "Sports")
+                Console.WriteLine(sportsQuestions.Dequeue());
+            if (currentCategory() == "Rock")
+                Console.WriteLine(rockQuestions.Dequeue());
+        }
+
+
+        private String currentCategory()
+        {
+            if (places[currentPlayer] == 0) return "Pop";
+            if (places[currentPlayer] == 4) return "Pop";
+            if (places[currentPlayer] == 8) return "Pop";
+            if (places[currentPlayer] == 1) return "Science";
+            if (places[currentPlayer] == 5) return "Science";
+            if (places[currentPlayer] == 9) return "Science";
+            if (places[currentPlayer] == 2) return "Sports";
+            if (places[currentPlayer] == 6) return "Sports";
+            if (places[currentPlayer] == 10) return "Sports";
+            return "Rock";
+        }
+
+        public bool wasCorrectlyAnswered()
+        {
+            if (inPenaltyBox[currentPlayer])
+            {
+                if (isGettingOutOfPenaltyBox)
+                {
+                    Console.WriteLine("Answer was correct!!!!");
+                    currentPlayer++;
+                    if (currentPlayer == players.Count) currentPlayer = 0;
+                    purses[currentPlayer]++;
+                    Console.WriteLine(players[currentPlayer]
+                            + " now has "
+                            + purses[currentPlayer]
+                            + " Gold Coins.");
+
+                    bool winner = didPlayerWin();
+
+                    return winner;
+                }
+                else
+                {
+                    currentPlayer++;
+                    if (currentPlayer == players.Count) currentPlayer = 0;
+                    return true;
+                }
+
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("Answer was corrent!!!!");
+                purses[currentPlayer]++;
+                Console.WriteLine(players[currentPlayer]
+                        + " now has "
+                        + purses[currentPlayer]
+                        + " Gold Coins.");
+
+                bool winner = didPlayerWin();
+                currentPlayer++;
+                if (currentPlayer == players.Count) currentPlayer = 0;
+
+                return winner;
+            }
+        }
+
+        public bool wrongAnswer()
+        {
+            Console.WriteLine("Question was incorrectly answered");
+            Console.WriteLine(players[currentPlayer] + " was sent to the penalty box");
+            inPenaltyBox[currentPlayer] = true;
+
+            currentPlayer++;
+            if (currentPlayer == players.Count) currentPlayer = 0;
+            return true;
+        }
+
+
+        private bool didPlayerWin()
+        {
+            return !(purses[currentPlayer] == 6);
+        }
+    }
+
+}
---
+++ BugsZero/GameRunner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugsZero
+{
+
+    public class GameRunner
+    {
+
+        private static bool notAWinner;
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            playGame(rand);
+        }
+
+        public static void playGame(Random rand)
+        {
+            Game aGame = new Game();
+
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+
+
+            do
+            {
+
+                aGame.roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+
+
+
+            } while (notAWinner);
+        }
+    }
+}
